3db41d1b7c440fd86c13e3a436b85a3edf90ae76
jyy <mianbaodiandelaoban@gmail.com>
2022-07-23 19:29:53 +0800

    feat:完善then方法

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ class MPromise {
       this.reject(e);
     }
   }
-  
+
   resolve(v) {
-    if(this.MPromiseState !== 'pending') return;
+    if (this.MPromiseState !== 'pending') return;
     this.MPromiseState = 'fulfilled';
     this.MPromiseResult = v;
     this.callbacks.forEach(callback => {
@@ -20,7 +20,7 @@ class MPromise {
   }
 
   reject(r) {
-    if(this.MPromiseState !== 'pending') return;
+    if (this.MPromiseState !== 'pending') return;
     this.MPromiseState = 'rejected';
     this.MPromiseResult = r;
     this.callbacks.forEach(callback => {
@@ -30,21 +30,32 @@ class MPromise {
 
   then(onResolve, onReject) {
     return new MPromise((resolve, reject) => {
-      if(this.MPromiseState === 'fulfilled') {
-        if(typeof onResolve !== 'function') return this.MPromiseResult;
-        const res = onResolve(this.MPromiseResult);
-        if(res instanceof MPromise) {
-          // ...
-        } else {
-          resolve(res);
+      const thenCallback = cb => {
+        try {
+          const res = cb(this.MPromiseResult);
+          if (res instanceof MPromise) {
+            // 如果返回值是promise对象，返回值为成功，新promise就是成功;返回值为失败，新promise就是失败
+            res.then(resolve, reject);
+          } else {
+            // 如果返回值非promise对象，新promise对象就是成功，值为此返回值
+            resolve(res);
+          }
+        } catch (e) {
+          reject(e);
         }
       }
-      if(this.MPromiseState === 'rejected') {
-        if(typeof onReject !== 'function') throw this.MPromiseResult;
-        onReject(this.MPromiseResult);
+
+      if (this.MPromiseState === 'fulfilled') {
+        thenCallback(onResolve);
       }
-      if(this.MPromiseState === 'pending') {
-        this.callbacks.push({onResolve, onReject});
+      if (this.MPromiseState === 'rejected') {
+        thenCallback(onReject);
+      }
+      if (this.MPromiseState === 'pending') {
+        this.callbacks.push({
+          onResolve: thenCallback.bind(this, onResolve),
+          onReject: thenCallback.bind(this, onReject)
+        });
       }
     });
   }
